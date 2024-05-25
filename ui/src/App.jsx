88c18c9776c4045a038223cfcb9848dfa6d12282
MarcGoulding <mgtojar@gmail.com>
2024-05-25 02:57:45 +0200

    Add components to ui

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -12,25 +12,13 @@ import {
 import CandlestickChart from './components/CandlestickChart';
 import { initializeApp } from "firebase/app";
 import { getFunctions, httpsCallable } from 'firebase/functions';
+import BtcMoversLongs from './components/BtcMoversLongs';
+import BtcMoversShorts from './components/BtcMoversShorts';
+import UsdMoversLongs from './components/UsdMoversLongs';
+import UsdMoversShorts from './components/UsdMoversShorts';
 
-import dotenv from 'dotenv';
-// Load environment variables from .env
-dotenv.config();
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "cripto-tech.firebaseapp.com",
-  projectId: "cripto-tech",
-  storageBucket: "cripto-tech.appspot.com",
-  messagingSenderId: "383277567591",
-  appId: "1:383277567591:web:ba304c62e1564238e08ec2",
-  measurementId: "G-JYXEHYJ8QH"
-};
-export const firebaseApp = initializeApp(firebaseConfig, 'technex');
-const functions = getFunctions(firebaseApp);
 
-export const getDataFunction = httpsCallable(functions, 'data');
-export const getPrecisionFunction = httpsCallable(functions, 'precision');
 
 export default function App() {
 
@@ -41,56 +29,20 @@ export default function App() {
   const [SL, setSL] = useState(-1);
   const [TP, setTP] = useState(-1);
 
-  async function fetchPrecision(symbol) {
-    try {
-      const response = getPrecisionFunction({ symbol });
-      console.log(response.data);
-      setPrecision(response.data);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-  async function fetchData(symbol, interval, chart_type = "OHLC") {
-    console.log("Fetching ", symbol, interval, chart_type);
-
-    try {
-
-      const response = await getDataFunction({ symbol, interval, chart_type });
-      // Set the retrieved data to state
-      console.log(response.data);
-      setData(response.data);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
-  useEffect(() => {
-    fetchData(symbol, '4h');
-    fetchPrecision(symbol);
-  }, []);
-
-  useEffect(() => {
-    fetchData(symbol, '4h');
-    fetchPrecision(symbol);
-  }, [symbol]);
 
   return (
     <>
       <Heading>2 Cycles</Heading>
-      <Box w="70vw">
-        <CandlestickChart
-          data={data}
-          symbol={symbol}
-          interval='4h'
-          precision={precision}
-          Entry={Entry}
-          SL={SL}
-          TP={TP}
-        />
-      </Box>
-      <Flex>
+      <Flex justify="center" align="center">
         <CyclesUp />
         <CyclesDown />
+      </Flex>
+      <Heading>Top Movers</Heading>
+      <Flex justify="center" align="center">
+        <BtcMoversLongs />
+        <UsdMoversLongs />
+        <BtcMoversShorts />
+        <UsdMoversShorts />
       </Flex>
     </>
   );
